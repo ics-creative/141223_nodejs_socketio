99b3fc83c7453240e5c2443cb3bcc4127e158869
ics-ikeda <ikeda@ics-web.jp>
2018-06-05 21:03:24 +0900

    ソースコードを整形

--- js/controller.js
+++ js/controller.js
@@ -1,139 +1,136 @@
-var socket;
-if (typeof(io) != "undefined") {
-    socket = io.connect(location.origin);
+let socket;
+if (typeof(io) != 'undefined') {
+  socket = io.connect(location.origin);
 } else {
-    // ioオブジェクトが存在しない時にエラーにならない設定
-    socket = {
-        emit: function () {}, on: function () {}
+  // ioオブジェクトが存在しない時にエラーにならない設定
+  socket = {
+    emit: function () {
+    }, on: function () {
     }
+  };
 }
 
-var roomID;
+let roomID;
 $(function () {
-    var consoleText = $("#consoleText");
-    var windowDom = $(window);
-    var controllerWindow = $("#controllerWindow");
-    var controllPoint = $("#controllPoint");
-    var controllerWidth;
-    var controllerHeight;
-    var CONTROLLER_MARGIN = 10;
+  const windowDom = $(window);
+  const controllerWindow = $('#controllerWindow');
+  const controllPoint = $('#controllPoint');
+  let controllerWidth;
+  let controllerHeight;
+  const CONTROLLER_MARGIN = 10;
 
-    // ペアリングボタンをタップしたら、ペアリングを開始
-    $("#pairingButton").click(function () {
-        startPairing();
+  // ペアリングボタンをタップしたら、ペアリングを開始
+  $('#pairingButton').click(function () {
+    startPairing();
+  });
+
+  // ペアリングに成功
+  socket.on('successPairing', function () {
+    showControllWindow();
+    $('#pairingIDText').text('Paring ID:' + roomID);
+  });
+
+  // PCとのペアリングに失敗
+  socket.on('failPairingWithPC', function () {
+    $('#pairingButton').show();
+    $('#paringMessage').hide();
+  });
+
+  // ウィンドウのリサイズ
+  windowDom.resize(resizeHandler);
+  resizeHandler();
+
+  // debug
+  //socket.emit("pairingFromController", {
+  //    "roomID": 1
+  //});
+
+  $('#room');
+
+  function startPairing() {
+    $('#pairingButton').hide();
+    $('#paringMessage').show();
+
+    roomID = $('#roomID').val();
+    socket.emit('pairingFromController', {
+      'roomID': roomID
+    });
+  }
+
+  function showControllWindow() {
+    $('#pairingWindow').animate({
+      'top': '0'
+    }, '1s', 'swing');
+
+    $('#pairingWindow').remove();
+    controllerWindow.animate({
+      'top': '50%'
+    }, '1s', 'swing');
+  }
+
+  function resizeHandler() {
+    controllerWidth = windowDom.width() - CONTROLLER_MARGIN * 2;
+    controllerHeight = windowDom.height() - CONTROLLER_MARGIN * 2;
+    controllerWindow.css({
+      'width': controllerWidth + 'px',
+      'height': controllerHeight + 'px',
+      'margin-left': '-' + controllerWidth / 2 + 'px',
+      'margin-top': '-' + controllerHeight / 2 + 'px'
+    });
+  }
+
+  const isTouch = ('ontouchstart' in window);
+  const MOUSE_DOWN = isTouch ? 'touchstart' : 'mousedown';
+  const MOUSE_UP = isTouch ? 'touchend' : 'mouseup';
+  const MOUSE_MOVE = isTouch ? 'touchmove' : 'mousemove';
+  const MOUSE_OUT = isTouch ? 'touchout' : 'mouseleave';
+
+
+  let isDrag = false;
+
+  $(document).on(MOUSE_DOWN, function (mouseEvent) {
+    isDrag = true;
+    mouseActionHandler(mouseEvent, 'mouseDownFromControler');
+  });
+
+  $(document).on(MOUSE_MOVE, function (mouseEvent) {
+    event.preventDefault();
+    if (isDrag) {
+      mouseActionHandler(mouseEvent, 'mouseMoveFromControler');
+    }
+  });
+
+  $(document).on(MOUSE_UP + ' ' + MOUSE_OUT, function (mouseEvent) {
+    isDrag = false;
+    mouseActionHandler(mouseEvent, 'mouseUpFromControler');
+  });
+
+  /**
+   * マウスのアクションがあった時に実行される
+   * マウスイベントを受け取って、emitEventNameというデータをサーバーに送信する
+   */
+  function mouseActionHandler(mouseEvent, emitEventName) {
+    const eventX = (!isTouch) ? mouseEvent.pageX : event.changedTouches[0].pageX;
+    const eventY = (!isTouch) ? mouseEvent.pageY : event.changedTouches[0].pageY;
+    // 座標の正規化
+    const uvx = (eventX - CONTROLLER_MARGIN) / controllerWidth;
+    const uvy = (eventY - CONTROLLER_MARGIN) / controllerHeight;
+    // サーバーに送信
+    socket.emit(emitEventName, {
+      'eventUVX': uvx, 'eventUVY': uvy
     });
 
-    // ペアリングに成功
-    socket.on("successPairing", function () {
-        showControllWindow();
-        $("#pairingIDText").text("Paring ID:" + roomID);
-    });
-
-    // PCとのペアリングに失敗
-    socket.on("failPairingWithPC", function () {
-        $("#pairingButton").show();
-        $("#paringMessage").hide();
-    });
-
-    // ウィンドウのリサイズ
-    windowDom.resize(resizeHandler);
-    resizeHandler();
-
-    // debug
-    //socket.emit("pairingFromController", {
-    //    "roomID": 1
-    //});
-
-    $("#room")
-    function startPairing() {
-        $("#pairingButton").hide();
-        $("#paringMessage").show();
-
-        roomID = $("#roomID").val();
-        socket.emit("pairingFromController", {
-            "roomID": roomID
-        });
-    }
-
-    function showControllWindow() {
-        $("#pairingWindow").animate({
-            "top": "0"
-        }, "1s", "swing");
-
-        $("#pairingWindow").remove();
-        controllerWindow.animate({
-            "top": "50%"
-        }, "1s", "swing");
-    }
-
-    function resizeHandler() {
-        controllerWidth = windowDom.width() - CONTROLLER_MARGIN * 2;
-        controllerHeight = windowDom.height() - CONTROLLER_MARGIN * 2;
-        controllerWindow.css({
-            "width": controllerWidth + "px", "height": controllerHeight + "px", "margin-left": "-" + controllerWidth / 2 + "px", "margin-top": "-" + controllerHeight / 2 + "px"
-        })
-    }
-
-    if ("ontouchstart" in window) {
-        var isTouch = true;
-        var MOUSE_DOWN = "touchstart";
-        var MOUSE_UP = "touchend";
-        var MOUSE_MOVE = "touchmove";
-        var MOUSE_OUT = "touchout";
+    if (!isDrag) {
+      if (controllPoint.hasClass('on')) {
+        controllPoint.removeClass('on');
+      }
     } else {
-        var isTouch = false;
-        var MOUSE_DOWN = "mousedown";
-        var MOUSE_UP = "mouseup";
-        var MOUSE_MOVE = "mousemove";
-        var MOUSE_OUT = "mouseleave";
+      if (!controllPoint.hasClass('on')) {
+        controllPoint.addClass('on');
+      }
+      controllPoint.css({
+        left: eventX - 40 + 'px', top: eventY - 40 + 'px'
+      });
     }
-
-    var isDrag = false;
-    var lastIsDrag = false;
-
-    $(document).on(MOUSE_DOWN, function (mouseEvent) {
-        isDrag = true;
-        mouseActionHandler(mouseEvent, "mouseDownFromControler");
-    });
-
-    $(document).on(MOUSE_MOVE, function (mouseEvent) {
-        event.preventDefault();
-        if (isDrag) {
-            mouseActionHandler(mouseEvent, "mouseMoveFromControler");
-        }
-    });
-
-    $(document).on(MOUSE_UP + " " + MOUSE_OUT, function (mouseEvent) {
-        isDrag = false;
-        mouseActionHandler(mouseEvent, "mouseUpFromControler");
-    });
-
-    /**
-     * マウスのアクションがあった時に実行される
-     * マウスイベントを受け取って、emitEventNameというデータをサーバーに送信する
-     */
-    function mouseActionHandler(mouseEvent, emitEventName) {
-        var eventX = (!isTouch) ? mouseEvent.pageX : event.changedTouches[0].pageX;
-        var eventY = (!isTouch) ? mouseEvent.pageY : event.changedTouches[0].pageY;
-        // 座標の正規化
-        var uvx = (eventX - CONTROLLER_MARGIN) / controllerWidth;
-        var uvy = (eventY - CONTROLLER_MARGIN) / controllerHeight;
-        // サーバーに送信
-        socket.emit(emitEventName, {
-            "eventUVX": uvx, "eventUVY": uvy
-        });
-
-        if (!isDrag) {
-            if (controllPoint.hasClass("on")) {
-                controllPoint.removeClass("on");
-            }
-        } else {
-            if (!controllPoint.hasClass("on")) {
-                controllPoint.addClass("on");
-            }
-            controllPoint.css({
-                left: eventX - 40 + "px", top: eventY - 40 + "px"
-            });
-        }
-    }
+  }
 });
--- server.js
+++ server.js
@@ -1,136 +1,132 @@
 // サーバー構築
-const http = require("http");
+const http = require('http');
 // fsモジュールの読み込み
-const fs = require("fs");
+const fs = require('fs');
 // pathモジュールの読み込み
-const path = require("path");
+const path = require('path');
 // httpサーバーを立てる
 const server = http.createServer(requestListener);
 // httpサーバーを起動する。
 server.listen((process.env.PORT || 5000), () => {
-    console.log((process.env.PORT || 5000) + "でサーバーが起動しました");
+  console.log((process.env.PORT || 5000) + 'でサーバーが起動しました');
 });
+
 /**
  * サーバーにリクエストがあった際に実行される関数
  */
 function requestListener(request, response) {
-    // リクエストがあったファイル
-    const requestURL = request.url;
-    // リクエストのあったファイルの拡張子を取得
-    const extensionName = path.extname(requestURL);
-    // ファイルの拡張子に応じてルーティング処理
-    switch(extensionName)
-    {
-        case ".html":
-            readFileHandler(requestURL, "text/html", false, response);
-            break;
-        case ".css":
-            readFileHandler(requestURL, "text/css", false, response);
-            break;
-        case ".js":
-        case ".ts":
-            readFileHandler(requestURL, "text/javascript", false, response);
-            break;
-        case ".png":
-            readFileHandler(requestURL, "image/png", true, response);
-            break;
-        case ".jpg":
-            readFileHandler(requestURL, "image/jpeg", true, response);
-            break;
-        case ".gif":
-            readFileHandler(requestURL, "image/gif", true, response);
-            break;
-        default:
-            // どこにも該当しない場合は、index.htmlを読み込む
-            readFileHandler("/index.html", "text/html", false, response);
-            break;
-    }
+  // リクエストがあったファイル
+  const requestURL = request.url;
+  // リクエストのあったファイルの拡張子を取得
+  const extensionName = path.extname(requestURL);
+  // ファイルの拡張子に応じてルーティング処理
+  switch (extensionName) {
+    case '.html':
+      readFileHandler(requestURL, 'text/html', false, response);
+      break;
+    case '.css':
+      readFileHandler(requestURL, 'text/css', false, response);
+      break;
+    case '.js':
+    case '.ts':
+      readFileHandler(requestURL, 'text/javascript', false, response);
+      break;
+    case '.png':
+      readFileHandler(requestURL, 'image/png', true, response);
+      break;
+    case '.jpg':
+      readFileHandler(requestURL, 'image/jpeg', true, response);
+      break;
+    case '.gif':
+      readFileHandler(requestURL, 'image/gif', true, response);
+      break;
+    default:
+      // どこにも該当しない場合は、index.htmlを読み込む
+      readFileHandler('/index.html', 'text/html', false, response);
+      break;
+  }
 }
 
 /**
  * ファイルの読み込み
  */
 function readFileHandler(fileName, contentType, isBinary, response) {
-    // エンコードの設定
-    const encoding = !isBinary ? "utf8" : "binary";
-    const filePath = __dirname + fileName;
+  // エンコードの設定
+  const encoding = !isBinary ? 'utf8' : 'binary';
+  const filePath = __dirname + fileName;
 
-    fs.exists(filePath, function(exits) {
-        if(exits)
-        {
-            fs.readFile(filePath, {encoding: encoding}, function (error, data) {
-                if (error) {
-                    response.statusCode = 500;
-                    response.end("Internal Server Error");
-                } else {
-                    response.statusCode = 200;
-                    response.setHeader("Content-Type", contentType);
-                    if(!isBinary)
-                    {
-                        response.end(data);
-                    }
-                    else
-                    {
-                        response.end(data, "binary");
-                    }
-                }
-            });
+  fs.exists(filePath, function (exits) {
+    if (exits) {
+      fs.readFile(filePath, {encoding: encoding}, function (error, data) {
+        if (error) {
+          response.statusCode = 500;
+          response.end('Internal Server Error');
+        } else {
+          response.statusCode = 200;
+          response.setHeader('Content-Type', contentType);
+          if (!isBinary) {
+            response.end(data);
+          }
+          else {
+            response.end(data, 'binary');
+          }
         }
-        else
-        {
-            // ファイルが存在しない場合は400エラーを返す。
-            response.statusCode = 400;
-            response.end("400 Error");
-        }
-    });
+      });
+    }
+    else {
+      // ファイルが存在しない場合は400エラーを返す。
+      response.statusCode = 400;
+      response.end('400 Error');
+    }
+  });
 }
 
 // socket.ioの読み込み
-const socketIO = require("socket.io");
+const socketIO = require('socket.io');
 // サーバーでSocket.IOを使える状態にする
 const io = socketIO.listen(server);
 
 // サーバーへのアクセスを監視。クライアントからのアクセスがあったらコールバックが実行
-io.sockets.on("connection", (socket) => {
-    let roomID;
-    // メイン画面からのpairingFromMainというデータを受信（メイン画面のペアリング）
-    socket.on("pairingFromMain", (data) => {
-        roomID = data.roomID;
-        socket.join(roomID);
-        socket.emit("successLoginPC");
-    });
-    // メイン画面からのforcePairingFromMainというデータを受信（強制ペアリング）
-    socket.on("forcePairingFromMain", (data) => {
-        roomID = data.roomID;
-        socket.join(roomID);
-        socket.emit("successPairing");
-    });
-    // コントローラーからのpairingFromControllerというデータを受信（コントローラーのペアリングイベント）
-    socket.on("pairingFromController", (data) => {
-        roomID = data.roomID;
-        socket.join(roomID);
-        // ルームIDがroomIDのグループにsuccessPairingというデータを送信
-        io.sockets.to(roomID).emit("successPairing");
-    });
-    // コントローラーからmouseDownFromControlerというデータを受信（コントローラでマウスダウンイベントが発生）
-    socket.on("mouseDownFromControler", (data) => {
-        socket.to(roomID).broadcast.emit("mouseDownToMain", data);
-    });
-    // コントローラーからmouseMoveFromControlerというデータを受信（コントローラでマウスムーブイベントが発生）
-    socket.on("mouseMoveFromControler", (data) => {
-        socket.to(roomID).broadcast.emit("mouseMoveToMain", data);
-    });
-    // コントローラーからmouseUpFromControlerというデータを受信（コントローラでマウスアップイベントが発生）
-    socket.on("mouseUpFromControler", (data) => {
-        socket.to(roomID).broadcast.emit("mouseUpToMain", data);
-    });
+io.sockets.on('connection', (socket) => {
+  let roomID;
+  // メイン画面からのpairingFromMainというデータを受信（メイン画面のペアリング）
+  socket.on('pairingFromMain', (data) => {
+    roomID = data.roomID;
+    socket.join(roomID);
+    socket.emit('successLoginPC');
+  });
+  // メイン画面からのforcePairingFromMainというデータを受信（強制ペアリング）
+  socket.on('forcePairingFromMain', (data) => {
+    roomID = data.roomID;
+    socket.join(roomID);
+    socket.emit('successPairing');
+  });
+  // コントローラーからのpairingFromControllerというデータを受信（コントローラーのペアリングイベント）
+  socket.on('pairingFromController', (data) => {
+    roomID = data.roomID;
+    socket.join(roomID);
+    // ルームIDがroomIDのグループにsuccessPairingというデータを送信
+    io.sockets.to(roomID).emit('successPairing');
+  });
+  // コントローラーからmouseDownFromControlerというデータを受信（コントローラでマウスダウンイベントが発生）
+  socket.on('mouseDownFromControler', (data) => {
+    socket.to(roomID).broadcast.emit('mouseDownToMain', data);
+  });
+  // コントローラーからmouseMoveFromControlerというデータを受信（コントローラでマウスムーブイベントが発生）
+  socket.on('mouseMoveFromControler', (data) => {
+    socket.to(roomID).broadcast.emit('mouseMoveToMain', data);
+  });
+  // コントローラーからmouseUpFromControlerというデータを受信（コントローラでマウスアップイベントが発生）
+  socket.on('mouseUpFromControler', (data) => {
+    socket.to(roomID).broadcast.emit('mouseUpToMain', data);
+  });
 });
 // 接続エラー
-io.sockets.on("connect_error", (socket) => {
-    console.log("connect_error");
+io.sockets.on('connect_error', (socket) => {
+  console.log('connect_error');
 });
 // 接続終了
-io.sockets.on("disconnect", (socket) => {
-    socket.emit("disconnectEvent");
-    console.log("disconnecth");
+io.sockets.on('disconnect', (socket) => {
+  socket.emit('disconnectEvent');
+  console.log('disconnecth');
 });
